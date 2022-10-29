a0b7591cf8e250b69a9ed9ceef02b9e62c8e0e2a
skbarbee <barbee.s.k@gmail.com>
2022-10-29 18:36:26 -0400

    rough landing page mock

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,20 @@
+import React from "react"
+import { Container, Image, Row, Col } from "react-bootstrap"
+
+
 const Home = (props) => {
 	// const { msgAlert, user } = props
 	console.log('props in home', props)
-
+	const  picture = require("./shared/Mock-up.png") 
 	return (
 		<>
-			<h2>Home Page</h2>
+	
+	<Container fluid className="center">
+			<h1>Pet Match</h1>
+			<Image fluid  src={picture} />
+			<h3> Find your best friend, their 2nd best friend</h3>
+
+		</Container>
 		</>
 	)
 }
--- src/components/shared/Header.js
+++ src/components/shared/Header.js
@@ -46,7 +46,7 @@ const Header = ({ user }) => (
 	<Navbar bg='primary' variant='dark' expand='md'>
 		<Navbar.Brand>
             <Link to='/' style={linkStyle}>
-                Pet Match
+                PetMatch
             </Link>
         </Navbar.Brand>
 		<Navbar.Toggle aria-controls='basic-navbar-nav' />
