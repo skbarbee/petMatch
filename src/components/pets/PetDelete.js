416af320623d661de41af32bad31dc261b21e8d9
Emily (Bare) Bernard <77028684+ebare012882@users.noreply.github.com>
2022-10-31 10:48:07 -0500

    added PetShow PetDelete and PetUpdate in the FrontEnd

---
+++ src/components/pets/PetDelete.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { useParams } from 'react-router-dom'
+// import PetUpdate from "./PetUpdate";
+
+
+const PetDelete = ({ handleDeletePet }) => {
+    return (
+        <>
+            <button onClick={handleDeletePet}>Delete Cat</button>
+        </>
+    )
+}
+
+export default PetDelete 
