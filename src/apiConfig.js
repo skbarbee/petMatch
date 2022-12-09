e6b333d9e76489394c49743ad667e1ac2f3e2a73
Emily (Bare) Bernard <77028684+ebare012882@users.noreply.github.com>
2022-12-09 16:31:04 -0600

    added deployed link to apiConfid

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,12 +1,7 @@
 let apiUrl
 const apiUrls = {
-<<<<<<< HEAD
-	// YOU MUST CHANGE PRODUCTION URL WHEN DEPLOYING
-	production: '<replace_with_deployed_api_url>',
-=======
     // YOU MUST CHANGE PRODUCTION URL WHEN DEPLOYING
 	production: 'https://petmatchapi.fly.dev',
->>>>>>> 0c2aefa3a04d18d61d5a3f020f4372a0ce158047
 	development: 'http://localhost:8000',
 }
 
